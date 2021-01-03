aa1f5b634d4c45215424686d462794bb14c3b840
Shweta <shweta3047@gmail.com>
2021-01-03 20:54:16 +0530

    added validation

--- server/api/services/code.service.js
+++ server/api/services/code.service.js
@@ -1,135 +1,148 @@
 import fs from "fs";
 import { execFile, spawn, exec } from "child_process";
+import ValidationService from "./validation.service";
 const FILE_PATH = `./executable`;
 const FILE_NAME = `sample`;
 const INPUT_NAME = `input`;
 
-class CodeService{
-    async execute(code,input,lang){
-        try{
-            //writing code in file
-            const fileName=await this.writeFile(code,lang);
-            //writing input in file
-            const inputName=input ? await this.writeFile(input, "input") : null;
-            //writing command 
-            const command=await this.writeCommand(lang,fileName,input);
-            //executing code
-            const output=await this.execChild(command);
+class CodeService {
+  async execute(code, input, lang) {
+    try {
+      //validating code
+      const { isValid, message } = await ValidationService.execute(
+        code,
+        input,
+        lang,
+        id
+      );
+      if (!isValid) {
+        throw {
+          message,
+        };
+      }
 
-            setTimeout(async()=>{
-                await this.deleteFiles(fileName,inputName,lang);
-            },200)
+      //writing code in file
+      const fileName = await this.writeFile(code, lang);
+      //writing input in file
+      const inputName = input ? await this.writeFile(input, "input") : null;
+      //writing command
+      const command = await this.writeCommand(lang, fileName, input);
+      //executing code
+      const output = await this.execChild(command);
 
-            if (output) return output.toString();
+      setTimeout(async () => {
+        await this.deleteFiles(fileName, inputName, lang);
+      }, 200);
 
-        }catch(error){
-            throw { status: "404", message: error };
+      if (output) return output.toString();
+    } catch (error) {
+      throw { status: "404", message: error };
+    }
+  }
+
+  async writeFile(code, lang) {
+    try {
+      let fileName = lang === "input" ? `${INPUT_NAME}` : `${FILE_NAME}`;
+      switch (lang) {
+        case "javascript": {
+          fileName += ".js";
+          break;
         }
+        case "c++": {
+          fileName += ".cpp";
+          break;
+        }
+        case "python": {
+          fileName += ".py";
+          break;
+        }
+        case "input": {
+          fileName += ".txt";
+          break;
+        }
+        default: {
+        }
+      }
+
+      fs.writeFile(`${FILE_PATH}/${fileName}`, code, (err) => {
+        if (err) {
+          throw { message: err };
+        }
+      });
+      return fileName;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async writeCommand(lang, fileName, inputName) {
+    let command = "";
+    switch (lang) {
+      case "javascript": {
+        command = `node ${FILE_PATH}/${fileName}`;
+        break;
+      }
+      case "c++": {
+        command = `cd ${FILE_PATH} && g++ ${fileName} && a ${
+          inputName ? `< ${inputName}` : null
+        } && cd ..`;
+        break;
+      }
+      case "python": {
+        command = `cd ${FILE_PATH} && python ${fileName} && ${
+          inputName ? `< ${inputName}` : null
+        } && cd ..`;
+        break;
+      }
+      default: {
+        throw "Invalid language";
+      }
     }
 
-    async writeFile(code,lang){
-        try{
-            let fileName=lang==="input"?`${INPUT_NAME}`:`${FILE_NAME}`;
-            switch(lang){
-                case "javascript":{
-                    fileName+=".js";
-                    break;
-                }
-                case "c++":{
-                    fileName+=".cpp";
-                    break;
-                }
-                case "python":{
-                    fileName+=".py";
-                    break;
-                }
-                case "input":{
-                    fileName+=".txt";
-                    break;
-                }
-                default:{}    
-            }
+    return command;
+  }
 
-             fs.writeFile(`${FILE_PATH}/${fileName}`,code,(err)=>{
-                if (err){
-                    throw {message:err}
-                }
-            })
-            return fileName;
+  async execChild(command) {
+    return new Promise((resolve, reject) => {
+      const child = spawn(command, { shell: true });
+      child.stdout.on("data", (data) => {
+        resolve(data);
+      });
+      child.stderr.on("data", (data) => {
+        reject(data.toString());
+      });
+      child.on("error", (err) => {
+        throw { status: "404", message: err };
+      });
 
-        }catch(error){
-            throw error;
+      child.on("exit", (code, signal) => {
+        console.log("code: ", code);
+        console.log("signal: ", signal);
+      });
+    });
+  }
+
+  async deleteFiles(fileName, inputName, lang) {
+    fs.unlinkSync(`${FILE_PATH}/${fileName}`, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+    if (inputName) {
+      fs.unlinkSync(`${FILE_PATH}/${inputName}`, (err) => {
+        if (err) {
+          throw err;
         }
+      });
     }
-
-    async writeCommand(lang,fileName,inputName){
-        let command="";
-        switch(lang){
-            case "javascript":{
-                command=`node ${FILE_PATH}/${fileName}`;
-                break;
-            }
-            case "c++":{
-                command=`cd ${FILE_PATH} && g++ ${fileName} && a ${
-                    inputName?`< ${inputName}`:null
-                } && cd ..`;
-                break;
-            }
-            case "python":{
-                command=`cd ${FILE_PATH} && python ${fileName} && ${
-                    inputName?`< ${inputName}`:null
-                } && cd ..`;
-                break;
-            }
-            default:{
-                throw "Invalid language";
-            }
+    if (lang == "c++") {
+      fs.unlinkSync(`${FILE_PATH}/a.exe`, (err) => {
+        if (err) {
+          throw err;
         }
-
-        return command;
+      });
     }
-
-    async execChild(command){
-        return new Promise((resolve,reject)=>{
-            const child=spawn(command,{shell:true});
-            child.stdout.on("data",(data)=>{
-                resolve(data)
-            });
-            child.stderr.on("data",(data)=>{
-                reject(data.toString());
-            })
-            child.on("error",(err)=>{
-                throw {status:"404",message:err};
-            })
-
-            child.on("exit",(code,signal)=>{
-                console.log("code: ", code);
-                console.log("signal: ", signal)
-            })
-        })
-    }
-
-    async deleteFiles(fileName,inputName,lang){
-        fs.unlinkSync(`${FILE_PATH}/${fileName}`,(err)=>{
-            if(err){
-                throw err;
-            }
-        });
-        if(inputName){
-            fs.unlinkSync(`${FILE_PATH}/${inputName}`,(err)=>{
-                if(err){
-                    throw err;
-                }
-            })
-        }
-        if(lang=="c++"){
-            fs.unlinkSync(`${FILE_PATH}/a.exe`,(err)=>{
-                if(err){
-                    throw err;
-                }
-            })
-        }
-    }
+  }
 }
 
 export default new CodeService();
